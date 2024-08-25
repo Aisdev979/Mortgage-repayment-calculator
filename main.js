03209e7746ecde9588fa43264582dc12a95ddd9e
aisdev <aisosamatthew247@gmail.com>
2024-08-25 09:23:03 +0100

    Created the first html and javascript functionality for the monthly repayment radio active button, in subsequence time more functionality like one for the interest only will be add and styling will be add. Stay tuned for future updates.

---
+++ main.js
@@ -0,0 +1,24 @@
+const loan = document.querySelector('#amount');
+const term = document.querySelector('#term');
+const rate = document.querySelector('#rate');
+const monthly = document.querySelector('#monthly');
+const total = document.querySelector('#total');
+const result = document.querySelector('#result');
+
+const monthlyRepayment = () => {
+    const monthlyTerms = +term.value * 12;
+    const monthlyRate = +rate.value / 100 / 12;
+    let cal = (+loan.value * monthlyRate) * ((1 + monthlyRate) ** monthlyTerms) / (((1 + monthlyRate) ** monthlyTerms) - 1);
+    let monthlyCal = cal * 100;
+    monthlyCal = Math.floor(cal)
+    monthlyCal /= 100
+    monthly.textContent = monthlyCal;
+    let totalCal = cal * monthlyTerms;
+    totalCal *= 100;
+    totalCal = Math.floor(totalCal);
+    totalCal /= 100;
+    total.textContent = totalCal;
+}
+
+result.addEventListener('click', monthlyRepayment)
+
